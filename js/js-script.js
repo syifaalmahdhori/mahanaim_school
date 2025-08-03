d0e0756ba64f4bc19465e13ba0622f2a5477a0a9
syifaalmahdhori <122727316+syifaalmahdhori@users.noreply.github.com>
2025-08-03 10:09:56 +0700

    Add files via upload

---
+++ js/js-script.js
@@ -0,0 +1,81 @@
+// JavaScript Document
+function toggleMore() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more-text");
+  const btn = document.getElementById("read-more-btn");
+
+  if (moreText.style.display === "inline") {
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+    btn.textContent = "MORE";
+  } else {
+    moreText.style.display = "inline";
+    dots.style.display = "none";
+    btn.textContent = "LESS";
+  }
+}
+
+function toggleMoreElem() {
+  const dots = document.getElementById("dots-elem");
+  const moreText = document.getElementById("more-text-elem");
+  const btn = document.getElementById("read-more-btn-elem");
+
+  if (moreText.style.display === "inline") {
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+    btn.textContent = "MORE";
+  } else {
+    moreText.style.display = "inline";
+    dots.style.display = "none";
+    btn.textContent = "LESS";
+  }
+}
+
+function toggleMoreJHS() {
+  const dots = document.getElementById("dots-jhs");
+  const moreText = document.getElementById("more-text-jhs");
+  const btn = document.getElementById("read-more-btn-jhs");
+
+  if (moreText.style.display === "inline") {
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+    btn.textContent = "MORE";
+  } else {
+    moreText.style.display = "inline";
+    dots.style.display = "none";
+    btn.textContent = "LESS";
+  }
+}
+
+function toggleMoreSenior() {
+  const dots = document.getElementById("dots-senior");
+  const moreText = document.getElementById("more-text-senior");
+  const btn = document.getElementById("read-more-btn-senior");
+
+  if (moreText.style.display === "inline") {
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+    btn.textContent = "MORE";
+  } else {
+    moreText.style.display = "inline";
+    dots.style.display = "none";
+    btn.textContent = "LESS";
+  }
+}
+
+function toggleMoreVocational() {
+  const dots = document.getElementById("dots-vocational");
+  const moreText = document.getElementById("more-text-vocational");
+  const btn = document.getElementById("read-more-btn-vocational");
+
+  if (moreText.style.display === "inline") {
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+    btn.textContent = "MORE";
+  } else {
+    moreText.style.display = "inline";
+    dots.style.display = "none";
+    btn.textContent = "LESS";
+  }
+}
+
